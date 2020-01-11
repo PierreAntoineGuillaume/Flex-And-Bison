db9336812a00ddbd3c330d0d40f79a3d5d8ed6aa
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2020-01-11 12:20:15 +0100

    :white_check_mark: 2nd unit test

--- tests/symbol_table/src/reference_test.cpp
+++ tests/symbol_table/src/reference_test.cpp
@@ -8,11 +8,15 @@
 #include <boost/test/unit_test.hpp>
 #include "src/reference.h"
 
+
 using symbol::reference;
 
-BOOST_AUTO_TEST_CASE(ReferenceEquality) {
+BOOST_AUTO_TEST_CASE(ReferenceEquality)
+{
     reference r1("filename", 1, 1, 0);
     reference r2("filename", 1, 1, 0);
+    reference r3("other filename", 1, 1, 0);
     BOOST_CHECK_EQUAL(r1, r2);
+    BOOST_CHECK(r1 != r3);
 
 }
