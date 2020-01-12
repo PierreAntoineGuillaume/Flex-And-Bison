6d788b86529367016811c15daf3531013e963a02
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2020-01-12 16:53:16 +0100

    :art: easier construction for dual ast (left, symbol, right)

--- lib/ast/src/dual_ast.h
+++ lib/ast/src/dual_ast.h
@@ -22,7 +22,7 @@ namespace v1::ast::fb31
         ~dual_ast() override = default;
         T accept(ast_visitor<T> & visitor) override;
 
-        dual_ast(unsigned char symbol, astptr left, astptr right);
+        dual_ast(astptr left, unsigned char symbol, astptr right);
 
         ast<T> & left();
         ast<T> & right();
@@ -36,7 +36,7 @@ T v1::ast::fb31::dual_ast<T>::accept(v1::ast::fb31::ast_visitor<T> & visitor)
 }
 
 template <typename T>
-v1::ast::fb31::dual_ast<T>::dual_ast(unsigned char symbol, astptr left, astptr right)
+v1::ast::fb31::dual_ast<T>::dual_ast(astptr left, unsigned char symbol, astptr right)
         :   ast<T>(), left_child(left), right_child(right), symbol(symbol)
 {
 
