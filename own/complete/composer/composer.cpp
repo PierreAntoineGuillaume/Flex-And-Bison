78449b7fb349fea2cff8e721e2892c954e8fa8af
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2020-02-01 01:33:11 +0100

    :tada: initialization for composer

---
+++ own/complete/composer/composer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pierreantoine on 01/02/2020.
+//
+
+#include "composer.h"
---
+++ own/complete/composer/composer.h
@@ -0,0 +1,12 @@
+//
+// Created by pierreantoine on 01/02/2020.
+//
+
+
+#pragma once
+
+class composer
+{
+
+};
+
